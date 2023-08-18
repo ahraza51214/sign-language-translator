d70d1e1468a37852c7c4cab4ca29652a0ca3053e
Ali Hassan Naqvi <ahn@MacBook-Pro-tilhrende-Ali.local>
2023-08-18 11:06:43 +0200

    initial commit

---
+++ src/Views/Login.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../Components/Login/LoginForm";
+
+const Login = () => {
+    return(
+        <>
+            <h1>LOGIN</h1>
+            <LoginForm />
+        </>
+    )
+}
+
+export default Login;
