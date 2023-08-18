437dc6b2319b3f39bbe5ca7826733c5e40b3d6ba
Pau Go Si <paugosi@hotmail.com>
2023-08-18 14:47:16 +0200

    some main css

--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -2,10 +2,10 @@ import LoginForm from "../Components/Login/LoginForm";
 
 const Login = () => {
     return(
-        <>
+        <div id='login-header'>
             <h1>LOGIN</h1>
             <LoginForm />
-        </>
+        </div>
     )
 }
 
