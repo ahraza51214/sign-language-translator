c8dd55ed0316244c0e78c61a296743162e6f809a
Ali Hassan Naqvi <ahn@MacBook-Pro-tilhrende-Ali.local>
2023-08-22 12:58:06 +0200

    files structure

--- src/Components/Home/LoginForm.jsx
+++ src/Components/Home/LoginForm.jsx
@@ -1,15 +1,13 @@
-import { useForm } from 'react-hook-form';
-
 const LoginForm = () => {
-    return(
-        <>
-            <h2>Whats Your Name</h2>
-            <form>
-                <input type="text" placeholder="Enter Name" />
-                <button>Submit</button>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h2>Whats Your Name</h2>
+      <form>
+        <input type="text" placeholder="Enter Name" />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default LoginForm;
